188c4eb09d790ee2637e79eb45f7ab06bb708601
Inna Forkert <inna.forkert@gmail.com>
2024-01-25 15:16:19 +0200

    add slow flight

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,24 +1,23 @@
-import { AnimatedSprite, Container, useApp, useTick } from "@pixi/react";
-import { useEffect, useRef, useState } from "react";
-import { Assets, Texture, isMobile } from "pixi.js";
+import { AnimatedSprite, Container, useApp } from "@pixi/react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Assets, Texture } from "pixi.js";
 
 function Image() {
  const app = useApp();
- const speed = 1;
  const [width, height] = [app.screen.width, app.screen.height];
  const tiltAngle = 50; // even only
- const bound = [0, 29];
 
- const [rotation, setRotation] = useState(0);
+ //  const [rotation, setRotation] = useState(0);
  const [x, setX] = useState(width / 2);
- const [y, setY] = useState(height - 100);
+ //  const [y, setY] = useState(height - 100);
  const [frames, setFrames] = useState([]);
- const [isDebounced, setIsDebounced] = useState(false);
  const [frame, setFrame] = useState(0);
  const [isMoving, setIsMoving] = useState(false);
  const interval = useRef(null);
+ const moveXInterval = useRef(null);
+ //  const moveYInterval = useRef(null);
 
- useTick((delta) => setRotation((prev) => prev + 0.01 * delta));
+ const bound = useMemo(() => [0, 29], []);
 
  function setAngle(diff) {
   if (Math.abs(diff) < 10) {
@@ -27,7 +26,7 @@ function Image() {
   }
   setIsMoving(true);
   setTimeout(() => setIsMoving(false), 200);
-  interval.current = null;
+  clearInterval(interval.current);
   setFrame((prev) => {
    return calculateNextFrame(diff, prev);
   });
@@ -37,7 +36,7 @@ function Image() {
   const calculated = diff / tiltAngle;
   if (calculated > 29) return frame;
   const res = frame + Math.round(calculated);
-  console.log(diff, res);
+  // console.log(diff, res);
   if (res < bound[0]) {
    return bound[1] + res;
   }
@@ -49,38 +48,88 @@ function Image() {
   return res;
  }
 
- function mousemove(e) {
-  if (isDebounced) {
+ //  function calculateRotation(diffX, diffY) {
+ //   console.log(diffX, diffY);
+ //   if (diffX > 0) {
+ //    if (diffY >= 0) {
+ //     setRotation(0.5 * Math.PI + Math.atan(Math.abs(diffX / diffY)));
+ //     return;
+ //    }
+ //    setRotation(Math.atan(Math.abs(diffX / diffY)));
+ //    return;
+ //   }
+ //   if (diffY >= 0) {
+ //    setRotation(Math.PI + Math.atan(Math.abs(diffX / diffY)));
+ //    return;
+ //   }
+ //   setRotation(1.5 * Math.PI + Math.atan(Math.abs(diffX / diffY)));
+ //  }
+
+ function slowlyMoveX(diffX) {
+  const maxSpeed = 4;
+  clearInterval(moveXInterval.current);
+  clearInterval(interval.current);
+  setIsMoving(true);
+
+  if (Math.abs(diffX) < maxSpeed) {
+   setX((prev) => prev + diffX);
+   setIsMoving(false);
    return;
   }
-  setIsDebounced(true);
-  setTimeout(() => setIsDebounced(false), 50);
-  const diff = e.global.x - x;
-  setAngle(diff);
-  setX((prev) => prev + diff * speed);
 
-  // while (float < 1000) {
-  //  setTimeout(() => {
-  //   if (e.global.x !== x) {
-  //    setX((prev) => prev + diff * (speed / 1000));
-  //   }
-  //  }, float);
-  //  float = float + speed > 1000 ? 1000 : float + speed;
+  if (diffX > 0) {
+   setX((prev) => prev + maxSpeed);
+   moveXInterval.current = setTimeout(() => slowlyMoveX(diffX - maxSpeed));
+   return;
+  }
+
+  setX((prev) => prev - maxSpeed);
+  moveXInterval.current = setTimeout(() => slowlyMoveX(diffX + maxSpeed));
+ }
+
+ //  function slowlyMoveY(diffY) {
+ //   const maxSpeed = 10;
+ //   clearInterval(moveYInterval.current);
+
+ //   if (Math.abs(diffY) < maxSpeed) {
+ //    setY((prev) => prev + diffY);
+ //    return;
+ //   }
+
+ //   if (diffY > 0) {
+ //    setY((prev) => prev + maxSpeed);
+ //    moveYInterval.current = setTimeout(() => slowlyMoveY(diffY - maxSpeed), 50);
+ //    return;
+ //   }
+
+ //   setY((prev) => prev - maxSpeed);
+ //   moveYInterval.current = setTimeout(() => slowlyMoveY(diffY + maxSpeed), 50);
+ //  }
+
+ function mousemove(e) {
+  const diffX = e.global.x - x;
+  // const diffY = e.global.y - y;
+  setAngle(diffX);
+  slowlyMoveX(diffX);
+  // slowlyMoveY(diffY);
+  // if (Math.abs(diffX) > 10 || Math.abs(diffY) > 10) {
+  //  calculateRotation(diffX, diffY);
   // }
  }
 
  useEffect(() => {
+  const pause = 100;
   if (!isMoving) {
    if (frame > frames.length / 2) {
     if (frame + 3 > frames.length) {
-     interval.current = setTimeout(() => setFrame(0), 100);
+     interval.current = setTimeout(() => setFrame(0), pause);
      return;
     }
-    interval.current = setTimeout(() => setFrame((prev) => prev + 2), 100);
+    interval.current = setTimeout(() => setFrame((prev) => prev + 2), pause);
     return;
    }
    if (frame > 1) {
-    interval.current = setTimeout(() => setFrame((prev) => prev - 2), 100);
+    interval.current = setTimeout(() => setFrame((prev) => prev - 2), pause);
    }
   }
  }, [frame, frames.length, isMoving]);
@@ -109,6 +158,10 @@ function Image() {
   }
  }, [bound, frame]);
 
+ //  useEffect(() => {
+ // console.log(rotation);
+ //  }, [rotation]);
+
  if (frames.length === 0 || frame > bound[1] || frame < bound[0]) {
   return null;
  }
@@ -119,11 +172,12 @@ function Image() {
     eventMode="dynamic"
     onglobalmousemove={mousemove}
     x={x}
-    y={y}
+    y={height - 100}
     scale={0.5}
     anchor={0.5}
     textures={frames}
     currentFrame={frame}
+    // rotation={rotation}
    />
   </Container>
  );
